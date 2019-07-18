cd3f62c9b64322c46c029d2dfbf0d183a500b411
huangshaolin <huangshaolin.me@outlook.com>
2019-07-18 14:08:31 +0800

    test(tests/): add test case for b8.py

---
+++ tests/test_b8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import unittest
+import subprocess
+
+from context import b8
+
+
+class TestB8(unittest.TestCase):
+
+    def test_b8(self):
+        b8_path = b8.__file__
+        document_url = 'https://max.book118.com/html/2019/0127/6202200032002004.shtm'
+        result = subprocess.run(['python3', b8_path, document_url])
+        self.assertEqual(result.returncode, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
