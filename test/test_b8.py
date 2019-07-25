bb8e31bc95899f0d1f7b6733fe6ac2a5a8467b17
huangshaolin <huangshaolin.me@outlook.com>
2019-07-25 10:28:27 +0800

    refactor(.travis.yml, tests/*): refactor testing and linting for CI

--- test/test_b8.py
+++ test/test_b8.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-import os
 import unittest
 import subprocess
 
-from .context import b8
+import b8
 
 
 class TestB8(unittest.TestCase):
@@ -12,7 +11,7 @@ class TestB8(unittest.TestCase):
     def test_b8(self):
         b8_path = b8.__file__
         document_url = 'https://max.book118.com/html/2019/0127/6202200032002004.shtm'
-        result = subprocess.run(['python3', b8_path, document_url])
+        result = subprocess.run(['python3', b8_path, document_url], check=True)
         self.assertEqual(result.returncode, 0)
 
 
